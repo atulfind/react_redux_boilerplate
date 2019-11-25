17df88f4b56301e509ca28e55e0aef3ac2f706b0
atul <atulfind@gmail.com>
2019-11-25 18:14:03 +0530

    Updates

---
+++ precache-manifest.cc7ceb136528f52b026087f72c9e9cd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433031309ec56449df8da081479633a2",
+    "url": "/react_redux_boilerplate/index.html"
+  },
+  {
+    "revision": "ab2935d99881db64eca4",
+    "url": "/react_redux_boilerplate/static/css/main.775b7dfe.chunk.css"
+  },
+  {
+    "revision": "45a755fb341d633383aa",
+    "url": "/react_redux_boilerplate/static/js/2.ebb187ec.chunk.js"
+  },
+  {
+    "revision": "ab2935d99881db64eca4",
+    "url": "/react_redux_boilerplate/static/js/main.e8895b1f.chunk.js"
+  },
+  {
+    "revision": "c649b99867a2c8d2342c",
+    "url": "/react_redux_boilerplate/static/js/runtime-main.04c37d87.js"
+  }
+]);
